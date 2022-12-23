7c0c14750c9c6dae61e284b29759d67eabbb3697
mohamedhelmy2003 <121306433+mohamedhelmy2003@users.noreply.github.com>
2022-12-23 08:11:34 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+           
+            for (var i = 2; i <= 100; i++)
+            {
+                bool isprimenumber = true;
+                for (var j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    { 
+                        isprimenumber = false; break;
+
+                    }
+                }
+                if (isprimenumber)
+                {
+                    Console.WriteLine(i +"- ");
+                }
+            }
+        }
+    }
+}
